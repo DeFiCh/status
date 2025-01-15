749e04578c7da3d1170af4002b9a687439876ff7
Prasanna Loganathar <pvl@prasannavl.com>
2025-01-15 11:19:17 +0800

    Remove debug log

--- generate-html.sh
+++ generate-html.sh
@@ -48,7 +48,6 @@ check(){
     esac
 
     # Verify status and write files
-    echo "${name}: {rc} / ${expected_rc} // ${host} / ${id}"
     if [ "${rc}" -eq "${expected_rc}" ]; then
         echo "${name}" > "${TMP_DIR}/${id}.ok"
     else
